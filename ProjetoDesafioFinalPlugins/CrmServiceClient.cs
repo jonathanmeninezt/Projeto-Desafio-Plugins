bee87573c70e765f9a7de28edf2f6bd7d5963e50
Jonathan Meninez <jonathan.meninezt@gmail.com>
2023-11-10 16:43:09 -0300

    update preValidation - create preOperation plugin

---
+++ ProjetoDesafioFinalPlugins/CrmServiceClient.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+
+namespace MeuProjetoInicial {
+    internal class CrmServiceClient {
+        private string connectionStringCRM;
+
+        public CrmServiceClient(string connectionStringCRM) {
+            this.connectionStringCRM = connectionStringCRM;
+        }
+
+        internal Guid Create(Entity newEntity) {
+            throw new NotImplementedException();
+        }
+
+        internal EntityCollection RetrieveMultiple(FetchExpression fetchExpression) {
+            throw new NotImplementedException();
+        }
+    }
+}
