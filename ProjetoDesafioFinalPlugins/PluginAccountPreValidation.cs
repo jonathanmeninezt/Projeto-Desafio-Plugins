cba459c0cded33b19fd6f3bef55315e566a8903b
Jonathan Meninez <jonathan.meninezt@gmail.com>
2023-11-13 17:08:58 -0300

    ajuste campo telefone

--- ProjetoDesafioFinalPlugins/PluginAccountPreValidation.cs
+++ ProjetoDesafioFinalPlugins/PluginAccountPreValidation.cs
@@ -39,7 +39,7 @@ namespace PluginsTreinamento {
 
                 if (!entidadeContexto.Contains("jony_telefone")) // verifica se o atributo telephone1 não está presente no contexto
                 {
-                    throw new InvalidPluginExecutionException("Campo Telefone principal é obrigatório!!"); // exibe Exception de Erro
+                    throw new InvalidPluginExecutionException("Campo Telefone principal é obrigatório!"); // exibe Exception de Erro
                 }
             }
         }
